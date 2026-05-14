8d45a53ffe1f14c59fbb79f7dbcfab5fde2fb911
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-13 18:29:01 -0600

    style: set up dashboard layout

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenu } from "../shared/side-menu/side-menu";
 
 @Component({
   selector: 'dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
--- src/app/shared/side-menu/side-menu.ts
+++ src/app/shared/side-menu/side-menu.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.html',
 })
 export class SideMenu {
-
+  menuItems = routes
+    .map(r => r.children ?? [])
+    .flat()
+    .filter(r => r && r.path !== '**')
+    .filter(r => !r.path?.includes(':'))
 }
