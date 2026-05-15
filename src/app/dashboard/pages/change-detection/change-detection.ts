ad476bf1a4421c7dda1c5609254b20184e1c5207
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-14 20:28:25 -0600

    feat: compare properties and signals

--- src/app/dashboard/pages/change-detection/change-detection.ts
+++ src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { Title } from "../../../shared/title/title";
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'change-detection',
-  imports: [],
+  imports: [Title, JsonPipe],
   templateUrl: './change-detection.html',
 })
 export class ChangeDetection {
 
+  currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+
+  constructor() {
+
+    setTimeout(() => {
+
+
+        // this.frameworkAsProperty.name = 'React';
+        this.frameworkAsSignal.update( value => {
+          value.name = 'React';
+
+          return {...value};
+        })
+
+        console.log('Hecho');
+    }, 3000);
+
+
+  }
 }
