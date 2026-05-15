a1af9ed44540323dcb6ac7c9a586d1308ffceebd
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-14 20:50:50 -0600

    feat: use defer for adding placeholder while component is loading

--- src/app/dashboard/pages/defer-views/defer-views.ts
+++ src/app/dashboard/pages/defer-views/defer-views.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from "../../../shared/title/title";
+import { HeavyLoaderSlow } from "../../../shared/heavy-loaders/heavy-loader-slow/heavy-loader-slow";
 
 @Component({
   selector: 'defer-views',
-  imports: [],
+  imports: [Title, HeavyLoaderSlow],
   templateUrl: './defer-views.html',
 })
 export class DeferViews {
