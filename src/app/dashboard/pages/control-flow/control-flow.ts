dc18908eec8b8996f42c33434931225ac47192b6
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-14 20:24:23 -0600

    feat: create control flow page

--- src/app/dashboard/pages/control-flow/control-flow.ts
+++ src/app/dashboard/pages/control-flow/control-flow.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Title } from '../../../shared/title/title';
+
+type Grade = 'A' | 'B' | 'F';
 
 @Component({
   selector: 'control-flow',
-  imports: [],
+  imports: [Title],
   templateUrl: './control-flow.html',
 })
 export class ControlFlow {
+  showContent = signal(true);
+  grade = signal<Grade>('A');
 
+  toggleContent() {
+    this.showContent.update(current => !current);
+  }
+
+  frameworks = signal(['Angular','Vue','Svelte','Qwik','React']);
+  frameworks2 = signal([]);
 }
--- src/app/shared/title/title.ts
+++ src/app/shared/title/title.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'title',
+  selector: 'app-title',
   imports: [],
   templateUrl: './title.html',
 })
 export class Title {
-
+  title = input.required<String>();
 }
