1457d07ec51a78d96bd49baf93652d9c693fd182
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-15 20:16:58 -0600

    feat: create fast loader component

--- src/app/shared/heavy-loaders/heavy-loader-fast/heavy-loader-fast.ts
+++ src/app/shared/heavy-loaders/heavy-loader-fast/heavy-loader-fast.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'heavy-loader-fast',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './heavy-loader-fast.html',
 })
 export class HeavyLoaderFast {
-
+  cssClass = input<string>();
 }
