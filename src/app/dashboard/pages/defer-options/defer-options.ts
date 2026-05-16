8cb5d625d5be520024d63692a20e3f0590c81f5b
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-15 20:17:19 -0600

    feat: test different defer options

--- src/app/dashboard/pages/defer-options/defer-options.ts
+++ src/app/dashboard/pages/defer-options/defer-options.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from "../../../shared/title/title";
+import { HeavyLoaderFast } from "../../../shared/heavy-loaders/heavy-loader-fast/heavy-loader-fast";
 
 @Component({
   selector: 'defer-options',
-  imports: [],
+  imports: [Title, HeavyLoaderFast],
   templateUrl: './defer-options.html',
 })
 export class DeferOptions {
