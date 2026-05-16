cf95125989190c21ec3d4b5e4873242899530869
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-15 22:41:39 -0600

    feat: fetch and show user data

--- src/app/dashboard/pages/user/user.ts
+++ src/app/dashboard/pages/user/user.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Title } from "../../../shared/title/title";
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../../../services/users.service';
+import { CommonModule } from '@angular/common';
+import { switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'user',
-  imports: [],
+  imports: [Title, CommonModule],
   templateUrl: './user.html',
 })
 export class User {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+  titleLabel = computed(() => {
+    if (this.user()) {
+      return `Información del usuario: ${this.user()?.first_name} ${
+        this.user()?.last_name
+      } `;
+    }
+
+    return 'Información del usuario';
+  });
 }
